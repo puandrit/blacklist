9300e6f3b84f3c36691b0c34e2a7cd26dbaee801
Francesco Molitierno <franc.molitierno@gmail.com>
2020-11-17 20:58:07 +0100

    fix config generation

--- setup.sh
+++ setup.sh
@@ -58,7 +58,6 @@ URI=$(uci get mmpbxrvsipnet.@profile[0].uri)
 USERNAME=$(uci get mmpbxrvsipnet.@profile[0].user_name)
 PRIMARY_PROXY=$(uci get mmpbxrvsipnet.@network[0].primary_proxy)
 REGISTRAR=$(uci get mmpbxrvsipnet.@network[0].primary_registrar)
-REALM=$(uci get mmpbxrvsipnet.@network[0].realm)
 PORT=$(uci get mmpbxrvsipnet.@network[0].primary_registrar_port)
 DOMAIN=$(uci get mmpbxrvsipnet.@network[0].domain_name)
 sed -i 's#URI#'"$URI"'#g' /etc/asterisk/sip.conf
@@ -66,7 +65,6 @@ sed -i 's#USERNAME#'"$USERNAME"'#g' /etc/asterisk/sip.conf
 sed -i 's#PASSWORD#'"$PASSWORD"'#g' /etc/asterisk/sip.conf
 sed -i 's#PRIMARY_PROXY#'"$PRIMARY_PROXY"'#g' /etc/asterisk/sip.conf
 sed -i 's#REGISTRAR#'"$REGISTRAR"'#g' /etc/asterisk/sip.conf
-sed -i 's#REALM#'"$REALM"'#g' /etc/asterisk/sip.conf
 sed -i 's#DOMAIN#'"$DOMAIN"'#g' /etc/asterisk/sip.conf
 sed -i 's#PORT#'"$PORT"'#g' /etc/asterisk/sip.conf
 #sed -i 's#;live_dangerously = no#live_dangerously = no#g' /etc/asterisk/asterisk.conf
@@ -89,7 +87,7 @@ if [ -n "$1" ]; then
 		echo Blacklist installation completed successfully: the app is active and running
 		echo To import the blacklist phonebook, digit './import-blacklist.sh' from the shell
 		EXTENSION="${URI/+/%2B}"
-		h=$(curl -s "http://blacklist.satellitar.it/ispro.php?exten=$EXTENSION") 
+		h=$(curl -s "http://blacklist.satellitar.it/ispro.php?exten=$EXTENSION")
 		echo "$h"
 	fi
 fi
